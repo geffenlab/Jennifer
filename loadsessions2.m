8a101e3bd0eab25e9d775f9fd53207c016300388
Unknown <blaje42@gmail.com>
2018-11-16 11:19:09 -0500

    Correcting mouse numbers

--- loadsessions2.m
+++ loadsessions2.m
@@ -20,7 +20,7 @@ if strcmp(opsin,'chr2')
             Sesh{1} = [1 2 3 4]; Sesh{2} = [1 2 3 4]; Sesh{3} = [1 2 3 4]; Sesh{4} = [1 2 3 4]; Sesh{5} = [1 2 3 4]; 
         elseif strcmp(loc,'ac')
             TITLE = 'Feedback ChR2 in AC';
-            MNum = [3122 3126];
+            MNum = [3222 3226];
             Sesh = cell(1,length(MNum));
             Sesh{1} = [5]; Sesh{2} = [5];
         end
@@ -28,13 +28,13 @@ if strcmp(opsin,'chr2')
 elseif strcmp(opsin,'archt') %ArchT
         if strcmp(loc,'ic')
             TITLE = 'Feedback ArchT in IC';
-            MNum = [3230 3231 3232];
+            MNum = [3230 3231 3234 3235];
             Sesh = cell(1,length(MNum));
-            Sesh{1} = [1 2 3 4]; Sesh{2} = [1 2 3 4]; Sesh{3} = [1 2 3 4];
+            Sesh{1} = [1 2 3 4]; Sesh{2} = [1 2 3 4]; Sesh{3} = [1 2 3 4]; Sesh{4} = [2 3 4];
         elseif strcmp(loc,'ac')
             TITLE = 'Feedback ArchT in AC';
-            MNum = [3130 3131 3132];
+            MNum = [3230 3231 3234 3235];
             Sesh = cell(1,length(MNum));
-            Sesh{1} = [5]; Sesh{2} = [5]; Sesh{3} = [5];
+            Sesh{1} = [5]; Sesh{2} = [5]; Sesh{3} = [5]; Sesh{4} = [5];
         end
  end
